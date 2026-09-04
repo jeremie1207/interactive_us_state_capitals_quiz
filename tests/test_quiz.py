5da02b2ed17607aed94bd8d6e4469e71487671ab
Jeremie Barhabazi <71102646+jeremie1207@users.noreply.github.com>
2026-09-04 10:29:14 -0400

    Refactor test_quiz.py for consistency and clarity

--- tests/test_quiz.py
+++ tests/test_quiz.py
@@ -1,11 +1,10 @@
 import pytest
 
 from interactive_us_state_capitals_quiz.quiz import (
-    #   USStateCapitalQuizCapitalQuiz,
     Question,
     Quiz,
+    # USStateCapitalQuizCapitalQuiz,
 )
-
 
 
 @pytest.fixture
@@ -13,43 +12,47 @@ def sample_question() -> Question:
     return Question(
         "What is the capital of California?",
         "Sacramento",
-        {
-            'A':"Phoenix",
-            'B': "Sacramento",
-            'C': "Denver",
-            'D': "Austin"
-        }
+        {"A": "Phoenix", "B": "Sacramento", "C": "Denver", "D": "Austin"},
     )
+
 
 @pytest.fixture
 def sample_quiz() -> Quiz:
     return Quiz(1)
 
+
 def test_set_user_answer_question(sample_question: Question) -> None:
-    expected_result: str = 'B'
+    expected_result: str = "B"
 
     sample_question.set_user_answer(expected_result)
 
     assert sample_question.user_answer == expected_result
 
+
 def test_set_user_answer_question_error(sample_question: Question) -> None:
-    with pytest.raises(ValueError,
-        match=r"Invalid answer : F, The answer should be A, B, C, or D"):
-            sample_question.set_user_answer('F')
+    with pytest.raises(
+        ValueError,
+        match=r"The answer should be A, B, C, or D",
+    ):
+        sample_question.set_user_answer("F")
+
 
 def test_check_answer_question_correct(sample_question: Question) -> None:
-    sample_question.set_user_answer('B')
+    sample_question.set_user_answer("B")
 
     assert sample_question.check_answer()
 
+
 def test_check_answer_question_incorrect(sample_question: Question) -> None:
-    sample_question.set_user_answer('D')
+    sample_question.set_user_answer("D")
 
     assert not sample_question.check_answer()
 
-def test_calculate_score_quiz(sample_quiz: Quiz,
-                            sample_question: Question) -> None:
-    sample_question.set_user_answer('B')
+
+def test_calculate_score_quiz(
+    sample_quiz: Quiz, sample_question: Question
+) -> None:
+    sample_question.set_user_answer("B")
     sample_quiz.questions = [sample_question]
     expected_score = 1
 
@@ -57,9 +60,11 @@ def test_calculate_score_quiz(sample_quiz: Quiz,
 
     assert sample_quiz.score == expected_score
 
-def test_get_score_in_percent_quiz(sample_quiz: Quiz,
-                            sample_question: Question) -> None:
-    sample_question.set_user_answer('B')
+
+def test_get_score_in_percent_quiz(
+    sample_quiz: Quiz, sample_question: Question
+) -> None:
+    sample_question.set_user_answer("B")
     sample_quiz.questions = [sample_question]
     expected_percent = 100
 
